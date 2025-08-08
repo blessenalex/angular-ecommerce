d761863efec04910720feb1c67a5d182b97ab47b
Blessen Alex <ablessen.95@gmail.com>
2025-08-08 18:39:59 +0530

    added fr localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-ecommerce';
+  divTitle = $localize`:This is the title of the main div:main-div`;
 }
